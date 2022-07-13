4cb85017df331023631900fcf115120052984238
Ifeanyi <72707830+Ifeanyi55@users.noreply.github.com>
2022-07-13 18:35:54 +0100

    A text summarizing application

---
+++ Summarizer.py
@@ -0,0 +1,21 @@
+import nltk
+from sumy.parsers.html import HtmlParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.nlp.stemmers import Stemmer
+
+def summarizer(url,sentence_count):
+    LANGUAGE = "english"
+    SENTENCE_COUNT = sentence_count
+    
+    url = url
+    
+    parser = HtmlParser.from_url(url,Tokenizer(LANGUAGE))
+
+    summarizer = LuhnSummarizer()
+    summarizer = LuhnSummarizer(Stemmer(LANGUAGE))
+    summarizer.stop_words = ("I","am","the","you","are","me","is","than","that")
+    
+    for sentence in summarizer(parser.document,SENTENCE_COUNT):
+    
+        print(sentence)
